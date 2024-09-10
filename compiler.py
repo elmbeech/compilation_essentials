1a796a0982614a7100b36223c88a95d9e5ae2e52
bue <epbucher@iu.edu>
2024-09-09 20:01:16 -0400

    state of the art.

--- compiler.py
+++ compiler.py
@@ -24,14 +24,16 @@ class Compiler:
         match e:
             case Constant(value):  # Lint; always leaf
                 constant = Constant(value)
-                print('RCO_EXP OUTPUT:', constant, [])
+                print('RCO_EXP OUTPUT constant:', (constant, []))
                 return (constant, []) # expression and enviroment (expr, [(Name, (expr, [(Name, expr)]))])
 
             case Call(Name('input_int'), []):  # Lint; maybe complex
                 inputint = Call(Name('input_int'), [])
                 if need_atomic:
                     tmp = Name(generate_name('tmp'))
+                    print('RCO_EXP OUTPUT input_int complex:', (tmp, [(tmp, inputint)]))
                     return (tmp, [(tmp, inputint)])  # packing all the temps outside and keep the atoms inside.
+                print('RCO_EXP OUTPUT input_int atom:', (inputint, []))
                 return (inputint, [])
 
             case UnaryOp(USub(), operand):  # Lint; maybe complex; operator, operand
@@ -39,7 +41,9 @@ class Compiler:
                 neg = UnaryOp(USub(), atm)
                 if need_atomic:
                     tmp = Name(generate_name('tmp'))
+                    print('RCO_EXP OUTPUT neg complex:', (tmp, l_tmp + [(tmp, neg)]))
                     return (tmp, l_tmp + [(tmp, neg)])
+                print('RCO_EXP OUTPUT neg atom:', (neg, l_tmp))
                 return (neg, l_tmp)
 
             case BinOp(left, Add(), right):  # Lint; maybe complex; expr, operator, expr
@@ -48,21 +52,26 @@ class Compiler:
                 add = BinOp(atm1, Add(), atm2)
                 if need_atomic:
                     tmp = Name(generate_name('tmp'))
+                    print('RCO_EXP OUTPUT add complex:', (tmp, l_tmp1 + l_tmp2 + [(tmp, add)]))
                     return (tmp, l_tmp1 + l_tmp2 + [(tmp, add)])
+                print('RCO_EXP OUTPUT add atom:', (add, l_tmp1 + l_tmp2))
                 return (add, l_tmp1 + l_tmp2)
 
             case BinOp(left, Sub(), right):  # Lint; maybe complex; expr, operator, expr
                 atm1, l_tmp1 = self.rco_exp(left, True)
-                atm2, l_tmp2 = self.rco_exp(rigt, True)
-                add = BinOp(atm1, Sub(), atm2)
+                atm2, l_tmp2 = self.rco_exp(right, True)
+                sub = BinOp(atm1, Sub(), atm2)
                 if need_atomic:
                     tmp = Name(generate_name('tmp'))
+                    print('RCO_EXP OUTPUT sub complex:', (tmp, l_tmp1 + l_tmp2 + [(tmp, sub)]))
                     return (tmp, l_tmp1 + l_tmp2 + [(tmp, sub)])
+                print('RCO_EXP OUTPUT sub atom:', (sub, l_tmp1 + l_tmp2))
                 return (sub, l_tmp1 + l_tmp2)
 
             case Name(var):  # Lvar
                 name = Name(var)
-                return name
+                print('RCO_STMT OUTPUT var name atom:', (name, []))
+                return (name, [])
 
             case _:
                 raise Exception('Error: Compiler.rco_exp case not yet implemented.')
@@ -74,19 +83,19 @@ class Compiler:
                 atm, l_tmp =  self.rco_exp(exp, True)
                 #l_stmt = [Expr(Call(Name('print'), [atm]))]
                 l_stmt = [Assign([varc], expc) for varc, expc in l_tmp] + [Expr(Call(Name('print'), [atm]))]
-                print('RCO_STMT OUTPUT print l_stmt:', l_stmt)
+                print('RCO_STMT OUTPUT print:', l_stmt)
                 return l_stmt
 
             case Expr(exp):  # Lint
                 atm, l_tmp =  self.rco_exp(exp, False)  # output: expression and enviroment
                 l_stmt = [Assign([varc], expc) for varc, expc in l_tmp]
-                print('RCO_STMT OUTPUT Expr l_stmt:', l_stmt)
+                print('RCO_STMT OUTPUT expr:', l_stmt)
                 return l_stmt
 
             case Assign([Name(var)], exp):  # Lvar
                 (atm, l_tmp) =  self.rco_exp(exp, False)  # output: expression and enviroment
                 l_stmt =  [Assign([varc], expc) for varc, expc in l_tmp] + [Assign([Name(var)], atm)]
-                print('RCO_STMT OUTPUT Assign l_stmt:', l_stmt)
+                print('RCO_STMT OUTPUT assign:', l_stmt)
                 return l_stmt
 
             case _:
@@ -102,7 +111,7 @@ class Compiler:
                 module = Module(l_stmt)
                 #ll_stmt = [self.rco_stmt(stmt) for stmt in body]
                 #module = Module(sum(ll_stmt, []))  # bue: what is this sum doing?!
-                print('RCO OUTPUT Module:', ast.dump(module))
+                print('RCO OUTPUT module:', ast.dump(module))
                 return module
 
             case _:
@@ -118,10 +127,12 @@ class Compiler:
         match e:
             case Constant(var):  # Lint atom
                 arg_var = Immediate(var)
+                print('SELECT_ARG OUTPUT constant:', arg_var)
                 return arg_var
 
             case Name(var):  # Lvar atom
                 arg_var = Variable(var)
+                print('SELECT_ARG OUTPUT var name:', arg_var)
                 return arg_var
 
             case _:
@@ -131,15 +142,12 @@ class Compiler:
         # workhorse
         print('SELECT_STMT INPUT:', s)
         match s:
-            case Assign([Name(var)], exp):  # Lvar expr (atm)
-                arg = self.select_arg(exp)
-                l_instr = []
-                l_instr.append( Instr('movq', [arg, Variable(var)]) )
-                return l_instr
-
             case Call(Name('input_int'), []):  # Lint  expr
+                arg_var = self.select_arg(Name(var))
                 l_instr = []
-                l_instr.append( Callq(label_name('input_int'), input_int) )
+                l_instr.append( Callq(label_name('input_int'), []) )
+                l_instr.append( Instr('movq', ['%rax', arg_var]) )
+                print('SELECT_STMT OUTPUT input_int:', l_instr)
                 raise l_instr
 
             case Assign([Name(var)], UnaryOp(USub(), operand)):  # Lint expr : exproperator, operand
@@ -149,6 +157,7 @@ class Compiler:
                 l_instr.append( Instr('movq', [arg_var, '%rax']) )
                 l_instr.append( Instr('negq', [operand]) )
                 l_instr.append( Instr('movq', ['%rax', arg_var]) )
+                print('SELECT_STMT OUTPUT neg:', l_instr)
                 return l_instr
 
             case Assign([Name(var)], BinOp(left, Add(), right)):  # Lint expr: expr, operator, expr
@@ -159,6 +168,7 @@ class Compiler:
                 l_instr.append( Instr('movq', [arg_left, '%rax']) )
                 l_instr.append( Instr('addq', [arg_right, '%rax']) )
                 l_instr.append( Instr('movq', ['%rax', arg_var]) )
+                print('SELECT_STMT OUTPUT add:', l_instr)
                 return l_instr
 
             case Assign([Name(var)], BinOp(left, Sub(), right)):  # Lint expr: expr, operator, expr
@@ -169,24 +179,41 @@ class Compiler:
                 l_instr.append( Instr('movq', [arg_left, '%rax']) )
                 l_instr.append( Instr('subq', [arg_right, '%rax']) )
                 l_instr.append( Instr('movq', ['%rax', arg_var]) )
+                print('SELECT_STMT OUTPUT sub:', l_instr)
                 return l_instr
 
             case Expr(Call(Name('print'), [exp])):  # Lint stmt
                 arg_exp = self.select_arg(exp)
-                print('BUE', arg_exp)
                 l_instr = []
-                l_instr.append( Callq(label_name('print_int'), arg_exp) )
+                l_instr.append( Instr('movq', [arg_exp, '%rdi']) )
+                l_instr.append( Callq(label_name('print_int'), []) )
+                print('SELECT_STMT OUTPUT print:', l_instr)
                 return l_instr
+
+            case Expr(exp):  # Lint stmt
+                arg_exp = self.select_arg(exp)
+                l_instr = []
+                l_instr.append( )
+                print('SELECT_STMT OUTPUT BUE exp:', arg_exp, l_instr)
+                return l_instr
+
+            case Assign([Name(var)], exp):  # Lvar stmt
+                arg = self.select_arg(exp)
+                l_instr = []
+                l_instr.append( Instr('movq', [arg, Variable(var)]) )
+                print('SELECT_STMT OUTPUT var name:', l_instr)
+                return l_instr
+
             case _:
                  raise Exception('Error: Compiler.select_stmt case not yet implemented.')
 
     def select_instructions(self, p: Module) -> X86Program:
-        print('SELECT_INSTRUCTIONS INPUT:', p)
+        print('SELECT_INSTRUCTIONS INPUT:', ast.dump(p))
         match p:
             case Module(body):
                 l_instr = [self.select_stmt(stmt) for stmt in body]
                 x86program = X86Program(sum(l_instr, []))
-                print('SELECT_INSTRUCTIONS INPUT:', x86program)
+                print('SELECT_INSTRUCTIONS OUTPUT x86program:', x86program)
                 return x86program
 
             case _:
