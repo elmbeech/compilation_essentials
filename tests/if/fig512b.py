d2182175c5275cb81e100f7977b9adf30aa7ce17
bue <epbucher@iu.edu>
2024-10-13 20:23:15 -0400

    fig512b test

---
+++ tests/if/fig512b.py
@@ -0,0 +1,10 @@
+x = input_int()
+y = input_int()
+
+#if (x == 0 if (x < 0) else x == 2):
+#    z = y + 2
+#else:
+#    z = y + 10
+#print(z)
+
+print(y + 2 if (x==0 if x < 1 else x == 2) else y + 10)
